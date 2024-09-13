49fa4d0aa6b2fcce43366abc757f77b3205b7846
clarissaindriana <clarissaindrianapramesti@gmail.com>
2024-09-13 15:53:25 +0700

    adding new secret action

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -116,5 +116,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://clarissa-indriana-mentalhealthtracker.pbp.cs.ui.ac.id", "https://clarissa-indriana-mentalhealthtracker.pbp.cs.ui.ac.id"]
